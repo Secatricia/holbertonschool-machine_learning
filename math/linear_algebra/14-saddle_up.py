5d03f9f811b9af7be612dd5fdfdb0ae74b18f3c8
Secatricia <lucie.delrue04@gmail.com>
2023-12-06 22:52:24 +0100

    Add task 14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Function that performs matrix multiplication"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """define np_matmul function"""
+    return np.dot(mat1, mat2)

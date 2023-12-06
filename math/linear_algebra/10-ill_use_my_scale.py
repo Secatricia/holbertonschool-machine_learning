fcf3ee225fe599328fb6da0f17acb0b8814ca160
Secatricia <lucie.delrue04@gmail.com>
2023-12-06 17:50:20 +0100

    Add task 10

---
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Function hat calculates the shape of a numpy.ndarray"""
+
+import numpy as np
+
+def np_shape(mat):
+    """define nb_shape function"""
+    return mat.shape

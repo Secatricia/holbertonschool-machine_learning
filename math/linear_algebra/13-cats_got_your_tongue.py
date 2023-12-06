18af403ef023dcf2ac632f21802b16205b74aa75
Secatricia <lucie.delrue04@gmail.com>
2023-12-06 22:44:52 +0100

    Change comment task 13

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """Function that concatenates two matrices along a specific axis"""
-
-
 import numpy as np
+
 
 def np_cat(mat1, mat2, axis=0):
     """define np_cat function"""

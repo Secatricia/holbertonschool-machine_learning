83ad4bb8cda554f21bbd142881a100b67cb11bee
Secatricia <lucie.delrue04@gmail.com>
2023-12-06 14:36:31 +0100

    Add task 7

---
+++ math/linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""function that concatenates two matrices along a specific axis"""
+
+
+def cat_matrices2D(mat1, mat3, axis=0):
+    """define cat_matrices2D function"""
+    result = []
+    concat_mat = []
+
+    if axis == 0:
+        for i in range(len(mat1)):
+            concat_mat = []
+            for j in range(len(mat1[0])):
+                concat_mat.append(mat1[i][j])
+            result.append(concat_mat)
+
+    elif axis == 1:
+        for i in range(len(mat1)):
+            concat_mat = mat1[i] + mat3[i]
+            result.append(concat_mat)
+
+    return result

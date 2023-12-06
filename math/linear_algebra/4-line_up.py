750780c3e9198ddefd2c6d571374d29fab52438c
Secatricia <lucie.delrue04@gmail.com>
2023-12-06 12:26:40 +0100

    Add task 4

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""function  that adds two arrays element-wise"""
+
+def add_arrays(arr1, arr2):
+    """function that define add_arrays"""
+    if len(arr1) != len(arr2):
+        return None
+
+    add = []
+
+    for i in range(len(arr1)):
+        add.append(arr1[i]+arr2[i])
+    return(add)

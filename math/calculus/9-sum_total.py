ac46a7ed578331078daebe29c603114ca4e4fe77
Secatricia <lucie.delrue04@gmail.com>
2023-12-12 10:45:52 +0100

    Add task 9

---
+++ math/calculus/9-sum_total.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""function that calculates \sum_{i=1}^{n} i^2"""
+
+def summation_i_squared(n):
+    """define summation_i_squared fuction"""
+
+    if not isinstance(n, int) or n<1:
+        return None
+
+    if n == 1:
+        return 1
+
+    return n*summation_i_squared(n-1)

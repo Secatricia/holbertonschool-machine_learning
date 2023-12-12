7bbfff3a6794600bcd8e240e8d50c290323ce6e9
Secatricia <lucie.delrue04@gmail.com>
2023-12-12 11:06:13 +0100

    Pycodestyle task 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-"""function that calculates \sum_{i=1}^{n} i^2"""
+"""function that calculates"\sum_{i=1}^{n} i^2""""
+
 
 def summation_i_squared(n):
     """define summation_i_squared fuction"""
 
-    if not isinstance(n, int) or n<1:
+    if not isinstance(n, int) or n < 1:
         return None
 
     if n == 1:

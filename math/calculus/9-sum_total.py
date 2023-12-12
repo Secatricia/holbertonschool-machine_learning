62d79047042d492a5b88853f9bc2d6527f2afb20
Secatricia <lucie.delrue04@gmail.com>
2023-12-12 14:44:03 +0100

    change condition task 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -7,9 +7,7 @@ def summation_i_squared(n):
 
     if not isinstance(n, int) or n < 1:
         return None
-
-    if n == 1:
+    elif n == 1:
         return 1
-
-    return n**2 + summation_i_squared(n-1)
-
+    else:
+        return n**2 + summation_i_squared(n-1)

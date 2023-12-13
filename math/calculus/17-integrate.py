99d7f7fe6fc9d6990eb4119eb0954d1bcb3d22d6
Secatricia <lucie.delrue04@gmail.com>
2023-12-13 16:05:54 +0100

    Add task 17

--- math/calculus/17-integrate.py
+++ math/calculus/17-integrate.py
@@ -4,18 +4,17 @@
 
 def poly_integral(poly, C=0):
     """define poly_integral function"""
-    if not isinstance(poly, list) or not all(isinstance(coeff, (int, float)) for coeff in poly):
-        return None
-    
-    if not isinstance(C, int):
-        return None
-
-    integral_coefficients = [C]
-    for i, coeff in enumerate(poly):
-        power = i + 1
-        if power == 1:
-            integral_coefficients.append(coeff / power)
+    if not isinstance(poly, list) or len(poly) < 1 or not isinstance(C, int):
+        result = None
+    else:
+        if poly == [0]:
+            result = [C]
         else:
-            integral_coefficients.append(coeff / power)
+            result = [C]
+            for x in range(len(poly)):
+                if poly[x] / (x + 1) == int(poly[x] / (x + 1)):
+                    result.append(int(poly[x] / (x + 1)))
+                else:
+                    result.append(poly[x] / (x + 1))
 
-    return integral_coefficients
+    return result

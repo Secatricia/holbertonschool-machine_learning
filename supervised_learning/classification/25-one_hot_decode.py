e54b6b37ebffabb2e8b88204b376e7f5ffca82aa
Secatricia <lucie.delrue04@gmail.com>
2024-02-10 22:10:42 +0100

    Add task 25

---
+++ supervised_learning/classification/25-one_hot_decode.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""converts a numeric label vector into a one-hot matrix"""
+
+
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """
+    Convert a one-hot matrix into a vector of labels.
+
+    Args:
+    one_hot: a one-hot encoded numpy.ndarray with shape (classes, m)
+
+    Returns:
+    A numpy.ndarray with shape (m, ) containing the numeric labels for each example, or None on failure
+    """
+    if not isinstance(one_hot, np.ndarray) or len(one_hot.shape) != 2:
+        return None
+
+    m = one_hot.shape[1]
+    decoded_labels = np.argmax(one_hot, axis=0)
+
+    return decoded_labels

daaca0c400248bbd7c88ef9fcd83c53f371cbcda
Secatricia <lucie.delrue04@gmail.com>
2024-02-17 23:17:56 +0100

    Add task 7

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -4,6 +4,7 @@
 
 import tensorflow.compat.v1 as tf
 
+
 def evaluate(X, Y, save_path):
     """
     evaluates the output of a neural network
@@ -21,7 +22,8 @@ def evaluate(X, Y, save_path):
     x = tf.get_collection('placeholders', scope=x_name)[0]
     y = tf.get_collection('placeholders', scope=y_name)[0]
     y_pred = tf.get_collection('tensors', scope='layer_2/BiasAdd:0')[0]
-    loss = tf.get_collection('tensors', scope='softmax_cross_entropy_loss/value:0')[0]
+    loss = tf.get_collection('tensors',
+                            scope='softmax_cross_entropy_loss/value:0')[0]
     accuracy = tf.get_collection('tensors', scope='Mean:0')[0]
 
     # Evaluate the model
@@ -30,6 +32,7 @@ def evaluate(X, Y, save_path):
         loader.restore(sess, save_path)
 
         # Run evaluation
-        pred, acc, cost = sess.run([y_pred, accuracy, loss], feed_dict={x: X, y: Y})
+        pred, acc, cost = sess.run([y_pred, accuracy, loss],
+                                   feed_dict={x: X, y: Y})
 
     return pred, acc, cost

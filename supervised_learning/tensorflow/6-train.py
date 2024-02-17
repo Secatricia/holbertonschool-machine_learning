275066f986652a288dc20b5889320e7d03cc1a37
Secatricia <lucie.delrue04@gmail.com>
2024-02-17 22:54:36 +0100

    pycodestyle task 6

--- supervised_learning/tensorflow/6-train.py
+++ supervised_learning/tensorflow/6-train.py
@@ -3,12 +3,18 @@
 
 import tensorflow.compat.v1 as tf
 
+create_placeholders = __import__('0-create_placeholders').create_placeholders
+forward_prop = __import__('2-forward_prop').forward_prop
+calculate_accuracy = __import__('3-calculate_accuracy').calculate_accuracy
+calculate_loss = __import__('4-calculate_loss').calculate_loss
+create_train_op = __import__('5-create_train_op').create_train_op
+
 
 def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations, alpha, iterations, save_path="/tmp/model.ckpt"):
     """
     builds, trains, and saves a neural network classifier
     """
-     tf.set_random_seed(0)
+    tf.set_random_seed(0)
 
     # Création des placeholders pour les données d'entrée et les étiquettes
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])

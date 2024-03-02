d2cec456a5cdd18a899cfb3c71c2d0fd35560f39
Secatricia <lucie.delrue04@gmail.com>
2024-03-02 20:47:58 +0100

    Add task 1

---
+++ supervised_learning/optimization/0-norm_constants.py
@@ -0,0 +1,12 @@
+#usr/bin/env python3
+
+
+import numpy as np
+import tensorflow.compat.v1. as tf
+
+def normalization_constants(X):
+
+    mean = np.mean(X, axis=0)
+    std_dev = np.std(X, axis=0)
+
+    return mean, std_dev

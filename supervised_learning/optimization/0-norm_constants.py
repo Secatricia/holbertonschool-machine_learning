6b107d67d6b2a745bbadd87e908fc67813a0037e
Secatricia <lucie.delrue04@gmail.com>
2024-03-02 20:54:37 +0100

    Task 0 pycodestyle comment

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -3,7 +3,9 @@
 
 import numpy as np
 
+
 def normalization_constants(X):
+    """calculates the normalization constants of a matrix"""
 
     mean = np.mean(X, axis=0)
     std_dev = np.std(X, axis=0)

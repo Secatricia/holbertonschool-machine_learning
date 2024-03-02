5c54b5f53b45d690c779cb99c1df28dde53748e4
Secatricia <lucie.delrue04@gmail.com>
2024-03-02 21:11:39 +0100

    Add task 1

---
+++ supervised_learning/optimization/1-normalize.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Normalization Constants"""
+
+
+import numpy as np
+
+
+def normalize(X, m, s):
+    """
+    Normalizes (standardizes) a matrix.
+    """
+    return (X - m) / s

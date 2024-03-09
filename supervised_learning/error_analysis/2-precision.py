fc44dc6526ea44a4c8d39c28fde4e9e5a7b017b5
Secatricia <lucie.delrue04@gmail.com>
2024-03-09 18:16:39 +0100

    add task 2

--- supervised_learning/error_analysis/2-precision.py
+++ supervised_learning/error_analysis/2-precision.py
@@ -6,9 +6,11 @@ import numpy as np
 
 
 def precision(confusion):
+    """ that calculates the precision for each class in a confusion matrix"""
     # Calculate precision for each class
+
     true_positives = np.diag(confusion)
     false_positives = np.sum(confusion, axis=0) - true_positives
     precision_per_class = true_positives / (true_positives + false_positives)
-    
+
     return precision_per_class

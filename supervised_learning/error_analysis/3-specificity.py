eed7ab742069c7b7a7a040e69498958d00b42ae5
Secatricia <lucie.delrue04@gmail.com>
2024-03-09 23:20:01 +0100

    Add pycodestyle task 3

--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -4,6 +4,7 @@
 
 import numpy as np
 
+
 def specificity(confusion):
     """that calculates the specificity for each class in a confusion matrix"""
 
@@ -14,6 +15,6 @@ def specificity(confusion):
     false_positives = np.sum(confusion, axis=0) - np.diag(confusion)
     specificity_per_class = true_negatives / \
                              (true_negatives + false_positives)
-    
+
     return specificity_per_class
 

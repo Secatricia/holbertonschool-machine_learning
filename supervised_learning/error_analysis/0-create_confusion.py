dd30a0a43dec3469c1ee6704056cb81a68017d97
Secatricia <lucie.delrue04@gmail.com>
2024-03-09 17:49:01 +0100

    Add task 0

---
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+"""Error Analysis"""
+
+
+import numpy as np
+
+
+def create_confusion_matrix(labels, logits):
+    """that creates a confusion matrix"""
+    # Convert one-hot encoded labels and logits to class indices
+    true_classes = np.argmax(labels, axis=1)
+    pred_classes = np.argmax(logits, axis=1)
+    
+    # Initialize confusion matrix
+    num_classes = labels.shape[1]
+    confusion = np.zeros((num_classes, num_classes))
+    
+    # Fill confusion matrix
+    for true_class, pred_class in zip(true_classes, pred_classes):
+        confusion[true_class][pred_class] += 1
+    
+    return confusion.astype(int)

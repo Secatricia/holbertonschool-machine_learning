5a5f217a14242db9c8a93c4fabf2c3831ad1b89f
Secatricia <lucie.delrue04@gmail.com>
2024-03-26 10:46:57 +0100

    Add task 2

---
+++ supervised_learning/keras/2-optimize.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""keras"""
+
+
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """sets up Adam optimization for a keras model"""
+    opt = K.optimizers.Adam(lr=alpha, beta_1=beta1, beta_2=beta2)
+    network.compile(
+        optimizer=opt,
+        loss='categorical_crossentropy',
+        metrics=['accuracy'])
+
+
+
+
+

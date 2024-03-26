3792fadaa89300b9e12ff4ef7cdea5ce58c8fe87
Secatricia <lucie.delrue04@gmail.com>
2024-03-26 10:35:17 +0100

    Add task 1

--- supervised_learning/keras/1-input.py
+++ supervised_learning/keras/1-input.py
@@ -12,7 +12,10 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     for i, (nodes, activation) in enumerate(zip(layers, activations)):
         kernel_reg = K.regularizers.l2(lambtha) if lambtha else None
         if i != len(layers) - 1:
-            x = K.layers.Dense(nodes, activation=activation, kernel_regularizer=kernel_reg)(x)
+            x = K.layers.Dense(
+                nodes,
+                activation=activation, 
+                kernel_regularizer=kernel_reg)(x)
             x = K.layers.Dropout(1 - keep_prob)(x)
         else:
             x = K.layers.Dense(nodes, activation=activation, kernel_regularizer=kernel_reg)(x)

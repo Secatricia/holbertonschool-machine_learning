6a1881d7698cce1ab236baef0bf08d82e29562c3
Secatricia <lucie.delrue04@gmail.com>
2024-03-26 17:42:30 +0100

    Pycodestyle task 11

--- supervised_learning/keras/11-config.py
+++ supervised_learning/keras/11-config.py
@@ -10,6 +10,7 @@ def save_config(network, filename):
     with open(filename, 'w') as f:
         f.write(network.to_json())
 
+
 def load_config(filename):
     """loads a model with a specific configuration"""
     with open(filename, 'r') as f:

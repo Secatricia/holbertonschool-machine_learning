a8d06e5dd807307c019763e33c65ad6a982f2c4a
Secatricia <lucie.delrue04@gmail.com>
2024-03-26 10:56:21 +0100

    Add task 4

---
+++ supervised_learning/keras/4-train.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""keras"""
+
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs, verbose=True, shuffle=False):
+    """ trains a model using mini-batch gradient descent"""
+    history = network.fit(
+        x=data, y=labels,
+        batch_size=batch_size, 
+        epochs=epochs, verbose=verbose, shuffle=shuffle)
+    return history

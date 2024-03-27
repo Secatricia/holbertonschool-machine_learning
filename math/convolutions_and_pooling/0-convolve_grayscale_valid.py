9b637f921f6a263e9eec4382ec7e4e2b483dc99f
Secatricia <lucie.delrue04@gmail.com>
2024-03-27 11:07:33 +0100

    Add task 0 to 6

---
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Convolutions and Pooling"""
+
+
+import numpy as np
+
+
+def convolve_grayscale_valid(images, kernel):
+    """performs a valid convolution on grayscale images"""
+
+

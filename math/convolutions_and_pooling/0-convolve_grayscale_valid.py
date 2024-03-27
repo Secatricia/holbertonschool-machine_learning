90bcec3c32032c2ede6b267c75d19934bdf280c5
Secatricia <lucie.delrue04@gmail.com>
2024-03-27 23:17:26 +0100

    Pycodestyle 0

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -13,6 +13,7 @@ def convolve_grayscale_valid(images, kernel):
 
     for i in range(h - kh + 1):
         for j in range(w - kw + 1):
-            convolved_images[:, i, j] = np.sum(images[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2))
+            convolved_images[:, i, j] = np.sum(
+                images[:, i:i+kh, j:j+kw] * kernel, axis=(1, 2))
 
     return convolved_images

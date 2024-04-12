f0cf9cd344bb8320616b8aa4b1fd088dccc19668
Secatricia <lucie.delrue04@gmail.com>
2024-04-12 11:25:12 +0200

    Pycodestyle task 0

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -26,7 +26,7 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     w_out = int((w_prev + 2 * pad_w - kw) / sw) + 1
     A = np.zeros((m, h_out, w_out, c_new))
     A_prev_pad = np.pad(
-        A_prev, ((0, 0) , (pad_h, pad_h), (
+        A_prev, ((0, 0), (pad_h, pad_h), (
             pad_w, pad_w), (0, 0)), mode='constant')
 
     for i in range(h_out):

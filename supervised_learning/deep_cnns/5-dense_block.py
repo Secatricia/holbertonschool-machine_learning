6c628ccacf223dc49c73e333bdfc72f08bf94431
Secatricia <lucie.delrue04@gmail.com>
2024-05-03 12:37:47 +0200

    Add task 5

--- supervised_learning/deep_cnns/5-dense_block.py
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -7,27 +7,35 @@ import tensorflow.keras as K
 
 def dense_block(X, nb_filters, growth_rate, layers):
     """Builds a dense block as described in Densely Connected Convolutional Networks."""
-    concat_layers = [X]
-    nb_filters_dense = nb_filters
+    # Initialize kernel
+    init = K.initializers.VarianceScaling(scale=2.0,
+                                          mode='fan_in',
+                                          distribution='truncated_normal',
+                                          seed=None)
 
-    for i in range(layers):
-        # Batch normalization
-        X = K.layers.BatchNormalization()(X)
-        # ReLU activation
-        X = K.layers.Activation('relu')(X)
-        # Convolution with bottleneck
-        X = K.layers.Conv2D(filters=4 * growth_rate, kernel_size=1,
-                            padding='same', kernel_initializer='he_normal')(X)
-        # Batch normalization
-        X = K.layers.BatchNormalization()(X)
-        # ReLU activation
-        X = K.layers.Activation('relu')(X)
-        # Convolution
-        X = K.layers.Conv2D(filters=growth_rate, kernel_size=3,
-                            padding='same', kernel_initializer='he_normal')(X)
-        # Concatenate with previous layers
-        concat_layers.append(X)
-        X = K.layers.concatenate(concat_layers, axis=-1)
-        nb_filters_dense += growth_rate
+    for layer in range(layers):
+        # Conv 1x1
+        bn = K.layers.BatchNormalization()(X)
+        relu = K.layers.ReLU()(bn)
+        conv = K.layers.Conv2D(filters=4 * growth_rate,
+                               kernel_size=(1, 1),
+                               strides=1,
+                               kernel_initializer=init)(relu)
 
-    return X, nb_filters_dense
+        # Conv 3x3
+        bn1 = K.layers.BatchNormalization()(conv)
+        relu1 = K.layers.ReLU()(bn1)
+        conv1 = K.layers.Conv2D(filters=growth_rate,
+                                kernel_size=(3, 3),
+                                padding="same",
+                                strides=1,
+                                kernel_initializer=init)(relu1)
+
+        # Concatenate
+        concatenate = K.layers.Concatenate()([X, conv1])
+
+        # Update X and filters
+        X = concatenate
+        nb_filters = nb_filters + growth_rate
+
+    return concatenate, nb_filters

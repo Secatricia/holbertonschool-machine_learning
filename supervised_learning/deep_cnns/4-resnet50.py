221e69a88aebfc6c2b8e7faa852f137c117703ad
Secatricia <lucie.delrue04@gmail.com>
2024-05-03 12:11:51 +0200

    Add task 4

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -16,16 +16,14 @@ def identity_block(A_prev, filters):
                                           distribution='truncated_normal', seed=None)
 
     # Input layer
-    inputs = K.Input(shape=(224, 224, 3))
+    inputs = K.Input(shape=(224, 224, 3), name='input_1')
 
     # Convolution 7x7
     conv7x7 = K.layers.Conv2D(64, kernel_size=(7, 7), strides=2, padding="same",
-                               kernel_initializer=init)(inputs)
-    bn_conv7x7 = K.layers.BatchNormalization()(conv7x7)
-    relu_conv7x7 = K.layers.ReLU()(bn_conv7x7)
-
-    # Max pooling layer
-    max_pool = K.layers.MaxPooling2D(pool_size=(3, 3), padding="same", strides=2)(relu_conv7x7)
+                               kernel_initializer=init, name='conv2d')(inputs)
+    bn_conv7x7 = K.layers.BatchNormalization(name='batch_normalization')(conv7x7)
+    relu_conv7x7 = K.layers.ReLU(name='re_lu')(bn_conv7x7)
+    max_pool = K.layers.MaxPooling2D(pool_size=(3, 3), padding="same", strides=2, name='max_pooling2d')(relu_conv7x7)
 
     # Stage 2
     id_block_1 = projection_block(max_pool, [64, 64, 256], s=1)
@@ -52,10 +50,10 @@ def identity_block(A_prev, filters):
     id_block_13 = identity_block(id_block_12, [512, 512, 2048])
 
     # Average Pooling
-    avg_pool = K.layers.AveragePooling2D(pool_size=(7, 7), strides=(1, 1))(id_block_13)
+    avg_pool = K.layers.AveragePooling2D(pool_size=(7, 7), strides=(1, 1), name='average_pooling2d')(id_block_13)
 
     # Output layer
-    output = K.layers.Dense(units=1000, activation='softmax', kernel_initializer=init)(avg_pool)
+    output = K.layers.Dense(units=1000, activation='softmax', kernel_initializer=init, name='dense')(avg_pool)
 
     # Define the model
     model = K.models.Model(inputs=inputs, outputs=output)

1a44e50cbef4d5daa1345e0e95489ca037083a0b
Secatricia <lucie.delrue04@gmail.com>
2024-05-03 11:23:16 +0200

    Pycodestyle task 3

--- supervised_learning/deep_cnns/3-projection_block.py
+++ supervised_learning/deep_cnns/3-projection_block.py
@@ -18,32 +18,33 @@ def projection_block(A_prev, filters, s=2):
     shortcut = K.layers.Conv2D(
         filters=F12, kernel_size=(1, 1),
         strides=(s, s), padding='valid',
-        kernel_initializer=init)(A_prev)
-    shortcut = K.layers.BatchNormalization(axis=3)(shortcut)
+        kernel_initializer=init, name='shortcut_conv')(A_prev)
+    shortcut = K.layers.BatchNormalization(axis=3, name='shortcut_batchnorm')(shortcut)
 
     # Main path
     conv1 = K.layers.Conv2D(
         filters=F11, kernel_size=(1, 1),
         strides=(s, s), padding='valid',
-        kernel_initializer=init)(A_prev)
-    conv1_bn = K.layers.BatchNormalization(axis=3)(conv1)
-    conv1_act = K.layers.Activation('relu')(conv1_bn)
+        kernel_initializer=init, name='conv1')(A_prev)
+    conv1_bn = K.layers.BatchNormalization(axis=3, name='conv1_bn')(conv1)
+    conv1_act = K.layers.ReLU(name='conv1_relu')(conv1_bn)
 
     conv2 = K.layers.Conv2D(
         filters=F3, kernel_size=(3, 3),
         strides=(1, 1), padding='same',
-        kernel_initializer=init)(conv1_act)
-    conv2_bn = K.layers.BatchNormalization(axis=3)(conv2)
-    conv2_act = K.layers.Activation('relu')(conv2_bn)
+        kernel_initializer=init, name='conv2')(conv1_act)
+    conv2_bn = K.layers.BatchNormalization(axis=3, name='conv2_bn')(conv2)
+    conv2_act = K.layers.ReLU(name='conv2_relu')(conv2_bn)
 
     conv3 = K.layers.Conv2D(
         filters=F12, kernel_size=(1, 1),
         strides=(1, 1), padding='valid',
-        kernel_initializer=init)(conv2_act)
-    conv3_bn = K.layers.BatchNormalization(axis=3)(conv3)
+        kernel_initializer=init, name='conv3')(conv2_act)
+    conv3_bn = K.layers.BatchNormalization(axis=3, name='conv3_bn')(conv3)
 
-    # Add shortcut value to main path
-    add = K.layers.Add()([conv3_bn, shortcut])
-    output = K.layers.Activation('relu')(add)
+    # Add shortcut value to main path, and pass it through a ReLU activation
+    add = K.layers.Add(name='add')([conv3_bn, shortcut])
+    output = K.layers.ReLU(name='output')(add)
 
     return output
+

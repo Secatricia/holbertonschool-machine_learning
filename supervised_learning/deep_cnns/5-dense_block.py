d1f7cfae7b47ca1131633c8d4ba11c4a7bcd89fb
Secatricia <lucie.delrue04@gmail.com>
2024-05-03 12:29:10 +0200

    Add task 5

--- supervised_learning/deep_cnns/5-dense_block.py
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -7,26 +7,32 @@ import tensorflow.keras as K
 
 def dense_block(X, nb_filters, growth_rate, layers):
     """Builds a dense block as described in Densely Connected Convolutional Networks."""
-    concat_layers = [X]
+    # He normal initialization
+    initializer = K.initializers.he_normal(seed=None)
+
+    # Save the input tensor for concatenation
+    concat_list = [X]
+
+    # Iterate through the number of layers
     for i in range(layers):
-        init = K.initializers.he_normal(seed=None)
+        # Compute the number of filters
+        filters = nb_filters + i * growth_rate
 
-        BN_1 = K.layers.BatchNormalization()(X)
-        activation_1 = K.layers.Activation('relu')(BN_1)
-        conv_1 = K.layers.Conv2D(filters=4 * growth_rate,
-                                  kernel_size=(1, 1),
-                                  padding='same',
-                                  kernel_initializer=init)(activation_1)
+        # Bottleneck layer
+        X = K.layers.BatchNormalization(axis=3)(X)
+        X = K.layers.Activation('relu')(X)
+        X = K.layers.Conv2D(filters * 4, kernel_size=1, padding='same', kernel_initializer=initializer)(X)
 
-        BN_2 = K.layers.BatchNormalization()(conv_1)
-        activation_2 = K.layers.Activation('relu')(BN_2)
-        conv_2 = K.layers.Conv2D(filters=growth_rate,
-                                  kernel_size=(3, 3),
-                                  padding='same',
-                                  kernel_initializer=init)(activation_2)
+        # 3x3 convolutional layer
+        X = K.layers.BatchNormalization(axis=3)(X)
+        X = K.layers.Activation('relu')(X)
+        X = K.layers.Conv2D(filters, kernel_size=3, padding='same', kernel_initializer=initializer)(X)
 
-        concat_layers.append(conv_2)
-        X = K.layers.concatenate(concat_layers)
-        nb_filters += growth_rate
+        # Append the output of the layer to the concatenation list
+        concat_list.append(X)
 
-    return X, nb_filters
+        # Concatenate the list
+        X = K.layers.concatenate(concat_list)
+
+    # Return the concatenated output and the number of filters
+    return X, filters

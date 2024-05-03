978986a21227f566f87c9f2afeba318794f1001a
Secatricia <lucie.delrue04@gmail.com>
2024-05-03 13:06:37 +0200

    Pycodestyle task 7

--- supervised_learning/deep_cnns/7-densenet121.py
+++ supervised_learning/deep_cnns/7-densenet121.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
-"""Deep Convolutional Architectures"""
+"""
+This module contains :
+A fuction that builds the DenseNet-121 architecture
 
-
+Function:
+   def densenet121(growth_rate=32, compression=1.0):
+"""
 import tensorflow.keras as K
 dense_block = __import__('5-dense_block').dense_block
 transition_layer = __import__('6-transition_layer').transition_layer
 
 
 def densenet121(growth_rate=32, compression=1.0):
-    """Builds the DenseNet-121 architecture"""
-# Init Kernels
+    """
+    builds the DenseNet-121 architecture
+    the input data will have shape (224, 224, 3)
+    """
+    # Init Kernels
     init = K.initializers.VarianceScaling(scale=2.0,
                                           mode='fan_in',
                                           distribution='truncated_normal',
